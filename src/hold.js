44b0f293fe5ff9c4d4cc6dfad78d27653591d72b
Brian Cavalier <brian@hovercraftstudios.com>
2015-11-13 14:48:42 -0500

    fix whitespace

--- src/hold.js
+++ src/hold.js
@@ -8,27 +8,27 @@ import MulticastSource from 'most/lib/source/MulticastSource';
 export default stream => new stream.constructor(new Hold(stream.source));
 
 class Hold extends MulticastSource {
-	constructor(source) {
-    super(source);
-    this.time = -Infinity;
-    this.value = void 0;
-    this.held = false;
-	}
-
-	add(sink) {
-    super.add(sink);
-    if (this.held) {
-        sink.event(this.time, this.value);
+    constructor(source) {
+        super(source);
+        this.time = -Infinity;
+        this.value = void 0;
+        this.held = false;
     }
-    return len;
-	}
 
-	event(t, x) {
-    if (t >= this.time) {
-        this.time = t;
-        this.value = x;
-        this.held = true;
+    add(sink) {
+        super.add(sink);
+        if (this.held) {
+            sink.event(this.time, this.value);
+        }
+        return len;
     }
-    super.event(t, x);
-	}
+
+    event(t, x) {
+        if (t >= this.time) {
+            this.time = t;
+            this.value = x;
+            this.held = true;
+        }
+        super.event(t, x);
+    }
 }
