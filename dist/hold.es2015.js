bf0b0c8963ba5ee2b207112a1736f0469e30d4c5
Andreas Ekeroot <andeke@gmail.com>
2016-11-21 18:23:36 +0100

    Added jsnext:main and module field to package.json. (#15)

---
+++ dist/hold.es2015.js
@@ -0,0 +1,46 @@
+import { MulticastSource } from '@most/multicast';
+
+// hold :: Stream a -> Stream a
+var index = stream => new stream.constructor(new MulticastSource(new Hold(stream.source)));
+
+class Hold {
+  constructor(source) {
+    this.source = source;
+    this.time = -Infinity;
+    this.value = void 0;
+  }
+
+  run(sink, scheduler) {
+    /* istanbul ignore else */
+    if (sink._hold !== this) {
+      sink._hold = this;
+      sink._holdAdd = sink.add;
+      sink.add = holdAdd;
+
+      sink._holdEvent = sink.event;
+      sink.event = holdEvent;
+    }
+
+    return this.source.run(sink, scheduler);
+  }
+}
+
+function holdAdd(sink) {
+  const len = this._holdAdd(sink);
+  /* istanbul ignore else */
+  if (this._hold.time >= 0) {
+    sink.event(this._hold.time, this._hold.value);
+  }
+  return len;
+}
+
+function holdEvent(t, x) {
+  /* istanbul ignore else */
+  if (t >= this._hold.time) {
+    this._hold.time = t;
+    this._hold.value = x;
+  }
+  return this._holdEvent(t, x);
+}
+
+export default index;
