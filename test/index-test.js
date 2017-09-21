eb91e3176a83b701034b241a816f9b61a9767c15
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-09-21 10:03:29 -0400

    fix: ensure the first event emits after run returns

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,7 @@
+import { assert, eq } from '@briancavalier/assert'
+import { at, chain, mergeArray, periodic, runEffects, scan, take, tap } from '@most/core'
 import { describe, it } from 'mocha'
-import { eq } from '@briancavalier/assert'
-import { at, chain, mergeArray, runEffects, take, tap } from '@most/core'
+
 import { hold } from '../src/index'
 import { newDefaultScheduler } from '@most/scheduler'
 
@@ -13,8 +14,33 @@ export const collect = s => {
 
 describe('hold', () => {
   it('should deliver most recent event to new observer', () => {
-    const s = hold(mergeArray([at(0, 0), at(1, 1), at(2, 2)]))
+    const s = hold(mergeArray([at(0, 0), at(10, 1), at(20, 2)]))
     return collect(chain(_ => s, take(1, s)))
       .then(eq([0, 1, 2]))
+  })
+
+  it(`does not emit on the same tick as run`, () => {
+    const stream = hold(scan(x => x + 1, 0, periodic(5)))
+    const scheduler = newDefaultScheduler()
+    const drain = s => runEffects(s, scheduler)
+
+    return drain(take(1, stream)).then(() => {
+      let called = false
+
+      stream.run(
+        {
+          event () {
+            called = true
+          },
+          error () {},
+          end () {}
+        },
+        scheduler
+      )
+
+      assert(!called)
+
+      return Promise.resolve().then(() => assert(called))
+    })
   })
 })
